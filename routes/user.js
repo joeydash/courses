67df88bb106ffbd80e8b73709411fa5e853d3463
vashugupta <rkstrvashu.8@gmail.com>
2020-06-20 01:22:57 +0530

    msg91

--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,8 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     uh.getGithub().then(result=>res.json(result)).catch(err=>res.send(err));
 });
+router.get('/otp', (req, res, next) => {
+    uh.sendSmsOtp().then(result=>res.json(result)).catch(err=>res.send(err));
+});
 
 module.exports = router;
