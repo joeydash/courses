f2bfc30a99cd76faeb7bc7816f5a62fc40401370
joeydash <joydassudipta@gmail.com>
2020-06-20 10:58:42 +0530

    bug removed

--- routes/user_helper.js
+++ routes/user_helper.js
@@ -10,7 +10,7 @@ let app = {
 let application = {
     sendSmsOtp: () => {
         return new Promise((resolve, reject) => {
-            fetch('https://api.msg91.com/api/v5/otp?authkey=332926ASt3V8aIVwSx5eeb95d4P1&template_id=5eec94dfd6fc05559d21b91a&mobile=919695553773&invisible=1&otp=1234&userip=IPV4 User IP&email=Email ID').then(res => res.json()).then(res => resolve(res)).catch(onError => reject(onError));
+            fetch('https://api.msg91.com/api/v5/otp?authkey=332926ASt3V8aIVwSx5eeb95d4P1&template_id=5eec94dfd6fc05559d21b91a&mobile=919695553773&invisible=1&otp=1234 User IP&email=Email ID').then(res => res.json()).then(res => resolve(res)).catch(onError => reject(onError));
         })
     }
 }
