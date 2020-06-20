28fe46af54c677496d4bcba632cd112b11f3c9d9
joeydash <joydassudipta@gmail.com>
2020-06-20 11:36:58 +0530

    bug removed

--- routes/user.js
+++ routes/user.js
@@ -2,27 +2,15 @@ const express = require('express');
 const uh = require('./user_helper')
 const router = express.Router();
 
-var API_KEY = 'YOUR_API_KEY';
-var DOMAIN = 'YOUR_DOMAIN_NAME';
-var mailgun = require('mailgun-js')({ apiKey: API_KEY, domain: DOMAIN });
 
-const data = {
-    from: 'Excited User <me@samples.mailgun.org>',
-    to: 'foo@example.com, bar@example.com',
-    subject: 'Hello',
-    text: 'Testing some Mailgun awesomeness!'
-};
-
-mailgun.messages().send(data, (error, body) => {
-    if (error) {
-        console.log(error);
-    }
-    console.log(body);
-});
 
 
 router.get('/', (req, res, next) => {
-    uh.getGithub().then(result=>res.json(result)).catch(err=>res.send(err));
+    uh.sendOTPMail().then(result=>res.json(result)).catch(err=>res.send(err));
+});
+
+router.get('/email_signup', (req, res, next) => {
+    uh.sendOTPMail().then(result=>res.json(result)).catch(err=>res.send(err));
 });
 
 module.exports = router;
--- routes/user_helper.js
+++ routes/user_helper.js
@@ -1,15 +1,36 @@
 const fetch = require('node-fetch');
+var API_KEY = '98631eee81199733066c20f5c2a88e41-9a235412-8a0a2a2e';
+var DOMAIN = 'shoptohome.co.in';
+var mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
+
 
 let app = {
+    sendOTPMail: () => {
+        return new Promise((resolve, reject) => {
+            const data = {
+                from: "OTP Service <otp@shoptohome.co.in>",
+                to: 'ae16b111@smail.iitm.ac.in',
+                subject: 'No-Reply: Your OTP',
+                text: 'You OTP is 1234'
+            };
+
+            mailgun.messages().send(data, (error, body) => {
+                if (error) {
+                    console.log(error);
+                    reject(error);
+                } else {
+                    // console.log(body);
+                    resolve(body);
+                }
+            });
+        })
+    },
     getGithub: () => {
         return new Promise((resolve, reject) => {
             fetch('https://api.github.com/users/joeydash').then(res => res.json()).then(res => resolve(res)).catch(onError => reject(onError));
         })
     }
 }
-
-
-
 
 
 module.exports = app;
