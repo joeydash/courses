cdfe0868f994733f19dd50a579927ec9a4f4ea44
vsoni101 <vs201100@gmail.com>
2020-06-21 10:01:09 +0530

    otp-verif-query

--- routes/user_helper.js
+++ routes/user_helper.js
@@ -2,6 +2,7 @@ const fetch = require('node-fetch');
 var API_KEY = '98631eee81199733066c20f5c2a88e41-9a235412-8a0a2a2e';
 var DOMAIN = 'shoptohome.co.in';
 var mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
+let otp = Math.floor(100000 + Math.random() * 900000);
 
 
 let app = {
@@ -9,9 +10,9 @@ let app = {
         return new Promise((resolve, reject) => {
             const data = {
                 from: "OTP Service <otp@shoptohome.co.in>",
-                to: 'ae16b111@smail.iitm.ac.in',
+                to: 'vs201400@gmail.com',
                 subject: 'No-Reply: Your OTP',
-                text: 'You OTP is 1234'
+                text: 'You OTP is ' + otp
             };
 
             mailgun.messages().send(data, (error, body) => {
