57fba5f2aa26a2cd18aec234d995a990970c0d7b
joeydash <joydassudipta@gmail.com>
2020-06-24 10:57:03 +0530

    bug removed

--- routes/user.js
+++ routes/user.js
@@ -4,15 +4,23 @@ const router = express.Router();
 
 
 router.get('/', (req, res, next) => {
-    uh.getGithub().then(result=>res.json(result)).catch(err=>res.send(err));
+    uh.getGithub().then(result => res.json(result)).catch(err => res.send(err));
 });
 router.get('/otp', (req, res, next) => {
-    uh.sendSmsOtp().then(result=>res.json(result)).catch(err=>res.send(err));
+    uh.sendSmsOtp().then(result => res.json(result)).catch(err => res.send(err));
 });
 router.post('/phone_sign_up', (req, res, next) => {
-    uh.phone_fullname_save(req.body.phone,req.body.name).then(result=>{
-        uh.sendSmsOtp(req.body.phone).then(result=>res.json(result)).catch(err=>res.json(err))
-          uh.verifyOtp(req.body.phone).then(result=>res.json(result)).catch(err=>res.json(err))
-    }).catch(err=>res.json(err));
+    uh.phone_save(req.body.phone).then(result => {
+        uh.sendSmsOtp(req.body.phone).then(result => res.json(result)).catch(err => res.json(err))
+    }).catch(err => res.json(err));
+});
+router.post('/verify_otp', (req, res, next) => {
+    uh.verifyOtp(req.body.phone, req.body.otp).then(result => {
+        if (result.type === "success") {
+            uh.phone_verified_db_change(req.body.phone).then(result => res.json(result)).catch(err => res.json(err))
+        } else {
+            res.json({"data": {"update_auth": {"affected_rows": 0}}})
+        }
+    }).catch(err => res.send(err));
 });
 module.exports = router;
--- routes/user_helper.js
+++ routes/user_helper.js
@@ -13,10 +13,10 @@ let app = {
     },
     verifyOtp: (phone, otp) => {
         return new Promise((resolve, reject) => {
-            fetch('https://api.msg91.com/api/v5/otp/verify?mobile=' + phone + '&otp= ' + otp + ' &authkey=332926ASt3V8aIVwSx5eeb95d4P1').then(res => res.json()).then(res => resolve(res)).catch(onError => reject(onError));
+            fetch('https://api.msg91.com/api/v5/otp/verify?mobile=' + phone + '&otp=' + otp + '&authkey=332926ASt3V8aIVwSx5eeb95d4P1').then(res => res.json()).then(res => resolve(res)).catch(onError => reject(onError));
         })
     },
-    phone_fullname_save: (phone, fullname) => {
+    phone_save: (phone) => {
         return new Promise((resolve, reject) => {
             fetch('https://lmsdb.herokuapp.com/v1/graphql', {
                 method: "post",
@@ -24,14 +24,34 @@ let app = {
                     'x-hasura-access-key': "joeydash"
                 },
                 body: JSON.stringify({
-                    query: `mutation MyMutation($phone: String = "", $fullname: String = "") {
-                              insert_auth(objects: {phone: $phone, fullname: $fullname}) {
+                    query: `mutation MyMutation($phone: String = "") {
+                              insert_auth(objects: {phone: $phone}, on_conflict: {update_columns: phone, constraint: auth_phone_carrier_key}) {
                                 affected_rows
                               }
                             }`,
                     variables: {
                         "phone": phone,
-                        "fullname": fullname
+                    }
+                })
+            }).then(res => res.json())
+                .then(res => resolve(res)).catch(err => reject(err))
+        })
+    },
+    phone_verified_db_change: (phone) => {
+        return new Promise((resolve, reject) => {
+            fetch('https://lmsdb.herokuapp.com/v1/graphql', {
+                method: "post",
+                headers: {
+                    'x-hasura-access-key': "joeydash"
+                },
+                body: JSON.stringify({
+                    query: `mutation MyMutation($phone: String = "") {
+                      update_auth(where: {phone: {_eq: $phone}}, _set: {phone_verified: true}) {
+                        affected_rows
+                      }
+                    }`,
+                    variables: {
+                        "phone": phone,
                     }
                 })
             }).then(res => res.json())
