11febf76a78b6f0b456de5f911bfb0fa4fcdccd2
vsoni101 <vs201100@gmail.com>
2020-06-24 20:57:38 +0530

    password hash complete

--- routes/user_helper.js
+++ routes/user_helper.js
@@ -3,6 +3,9 @@ const API_KEY = '98631eee81199733066c20f5c2a88e41-9a235412-8a0a2a2e';
 const DOMAIN = 'shoptohome.co.in';
 const mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 let app = {
     sendOTPMail: (email, otp) => {
@@ -19,32 +22,35 @@ let app = {
             });
         })
     },
-    email_signup: (email) => {
+    email_signup: (email, oldPass) => {
         return new Promise((resolve, reject) => {
-            fetch('https://lmsdb.herokuapp.com/v1/graphql', {
-                method: "post",
-                headers: {
-                    'x-hasura-admin-secret': 'joeydash'
-                },
-                body: JSON.stringify({
-                    query: `mutation MyMutation($otp: numeric = "", $email: String = "") {
-                              insert_auth(objects: {email: $email, otp: $otp}, on_conflict: {constraint: auth_email_carrier_key, update_columns: otp}) {
+            bcrypt.hash(oldPass, saltRounds, function (err, hash) {
+                fetch('https://lmsdb.herokuapp.com/v1/graphql', {
+                    method: "post",
+                    headers: {
+                        'x-hasura-admin-secret': 'joeydash'
+                    },
+                    body: JSON.stringify({
+                        query: `mutation MyMutation($email: String = "", $password: String = "", $otp: numeric = "") {
+                            insert_auth(objects: { email: $email, password: $password, otp: $otp }, on_conflict: { constraint: auth_email_carrier_key, update_columns: otp }) {
                                 affected_rows
                                 returning {
-                                  otp
-                                  email
+                                    email
+                                    otp
                                 }
-                              }
-                            }`,
-                    variables: {
-                        "otp": Math.floor(100000 + Math.random() * 900000),
-                        "email": email
-                    }
+                            }
+                        }`,
+                        variables: {
+                            "email": email,
+                            "password": hash,
+                            "otp": Math.floor(100000 + Math.random() * 900000)
+                        }
+                    })
                 })
-            })
-                .then(res => res.json())
-                .then(res => resolve(res))
-                .catch(err => reject(err));
+                    .then(res => res.json())
+                    .then(res => resolve(res))
+                    .catch(err => reject(err));
+            });
         })
     },
     mailVerify: (email, otp) => {
