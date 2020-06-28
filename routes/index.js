b8cdcd0bdf2a8cc0319a056f118d4a55775f0f10
joeydash <joydassudipta@gmail.com>
2020-06-28 15:38:35 +0530

    Merge branch 'Vaibhav' into vashu
    # Conflicts:
    #	package.json
    #	routes/index.js
    #	routes/user.js
    #	routes/user_helper.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+
+const uh = require('./user_helper')
+
+
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   fs.readFile('./public/docs/docs_list.json',"utf8", function(err, data) {
@@ -45,11 +49,11 @@ router.get('/email_signin', function (req, res, next) {
     res.render('email_signin/index');
 });
 
-router.get('/phone_signup', function(req, res, next) {
+router.get('/phone_signup', function (req, res, next) {
     res.render('new_user/index');
 });
 
-router.get('/phone_signin', function(req, res, next) {
+router.get('/phone_signin', function (req, res, next) {
     res.render('signin_user/index');
 });
 
@@ -58,7 +62,23 @@ router.get('/forum', function (req, res, next) {
     res.render('forum/index');
 })
 
-router.get('/test',(req, res, next)=>{
+router.get('/test', (req, res, next) => {
     res.render('home/index')
 });
+
+router.post('/webhook', (req, res, next) => {
+    uh.verifyAuthKey(req.get('Authorisation').split(' ')[1]).then(result => {
+        if (result.data.auth.length > 0) {
+            res.json({
+                "X-Hasura-User-Id": result.data.auth[0].id,
+                "X-Hasura-Role": "user",
+                "X-Hasura-Is-Owner": false
+            });
+        }else{
+            res.json({});
+        }
+    }).catch(err => res.json(err));
+});
+
+
 module.exports = router;
